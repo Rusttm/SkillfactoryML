036dcd49f46e43afae759f41cd7d27c3bd80e3b2
Rusttm <rust_mazh@mail.ru>
2021-04-23 16:47:41 +0300

    Update module3_5.py

--- module3_5.py
+++ module3_5.py
@@ -5,13 +5,18 @@ from sklearn.metrics import mean_squared_error, f1_score, accuracy_score, roc_cu
 from sklearn.model_selection import train_test_split
 from matplotlib import pyplot as plt
 
-data = load_boston()
-X, y = data['data'], data['target']
-X = np.hstack([np.ones(X.shape[0])[:, np.newaxis], X])
-theta = linreg_linear(X, y)
-y_pred = X.dot(theta)
 
 def print_regression_metrics(y_true, y_pred):
     mse = mean_squared_error(y_true, y_pred)
     rmse = np.sqrt(mse)
     print(f'MSE = {mse:.2f}, RMSE = {rmse:.2f}')
+
+def linreg_linear(X, y):
+    return True
+
+
+data = load_boston()
+X, y = data['data'], data['target']
+X = np.hstack([np.ones(X.shape[0])[:, np.newaxis], X])
+theta = linreg_linear(X, y)
+y_pred = X.dot(theta)
